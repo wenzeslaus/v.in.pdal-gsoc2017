2b61beb46b71b956f925ef5cbe3606d74091aa9f
ptschrum <ptschrum@ncsu.edu>
2017-07-08 20:44:01 +0200

    Added files for new class PdalVectorMapWriter, which extends pdal::Writer. Note, as of this commit the functionality is not correct. This is a Work In Progress commit.

---
+++ PdalVectorMapWriter.cpp
@@ -0,0 +1,99 @@
+// PdalVectorMapWriter.cpp
+// Adapted from https://www.pdal.io/tutorial/writing-writer.html#id1
+
+#include "PdalVectorMapWriter.hpp"
+#include <pdal/pdal_macros.hpp>
+#include <pdal/util/FileUtils.hpp>
+#include <pdal/util/ProgramArgs.hpp>
+
+namespace pdal
+{
+  static PluginInfo const s_info = PluginInfo(
+    "writers.PdalVectorMapWriter",
+    "Custom Writer to write a GRASS Vector Map",
+    "http://no/documentation/yet" );
+
+  CREATE_SHARED_PLUGIN(1, 0, PdalVectorMapWriter, Writer, s_info);
+
+  std::string PdalVectorMapWriter::getName() const { return s_info.name; }
+
+  struct FileStreamDeleter
+  {
+    template <typename T>
+    void operator()(T* ptr)
+    {
+      if (ptr)
+      {
+        ptr->flush();
+        FileUtils::closeFile(ptr);
+      }
+    }
+  };
+
+
+  void PdalVectorMapWriter::addArgs(ProgramArgs& args)
+  {
+    // setPositional() Makes the argument required.
+    args.add("filename", "Output filename", m_filename).setPositional();
+    args.add("newline", "Line terminator", m_newline, "\n");
+    args.add("datafield", "Data field", m_datafield, "UserData");
+    args.add("precision", "Precision", m_precision, 3);
+  }
+
+  void PdalVectorMapWriter::initialize()
+  {
+    m_stream = FileStreamPtr(FileUtils::createFile(m_filename, true),
+      FileStreamDeleter());
+    if (!m_stream)
+    {
+      std::stringstream out;
+      out << "writers.mywriter couldn't open '" << m_filename <<
+        "' for output.";
+      throw pdal_error(out.str());
+    }
+  }
+
+  void PdalVectorMapWriter::ready(PointTableRef table)
+  {
+    m_stream->precision(m_precision);
+    *m_stream << std::fixed;
+
+    Dimension::Id d = table.layout()->findDim(m_datafield);
+    if (d == Dimension::Id::Unknown)
+    {
+      std::ostringstream oss;
+      oss << "Dimension not found with name '" << m_datafield << "'";
+      throw pdal_error(oss.str());
+    }
+
+    m_dataDim = d;
+
+    *m_stream << "#X:Y:Z:MyData" << m_newline;
+  }
+
+
+  void PdalVectorMapWriter::write(PointViewPtr view)
+  {
+      for (PointId idx = 0; idx < view->size(); ++idx)
+      {
+        double x = view->getFieldAs<double>(Dimension::Id::X, idx);
+        double y = view->getFieldAs<double>(Dimension::Id::Y, idx);
+        double z = view->getFieldAs<double>(Dimension::Id::Z, idx);
+        unsigned int myData = 0;
+
+        if (!m_datafield.empty()) {
+          myData = (int)(view->getFieldAs<double>(m_dataDim, idx) + 0.5);
+        }
+
+        *m_stream << x << ":" << y << ":" << z << ":"
+          << myData << m_newline;
+      }
+  }
+
+
+  void PdalVectorMapWriter::done(PointTableRef)
+  {
+    m_stream.reset();
+  }
+
+}
---
+++ PdalVectorMapWriter.hpp
@@ -0,0 +1,40 @@
+// PdalVectorMapWriter.hpp
+// Adapted from https://www.pdal.io/tutorial/writing-writer.html#the-header
+
+#pragma once
+
+#include <pdal/Writer.hpp>
+
+#include <string>
+
+namespace pdal{
+
+  typedef std::shared_ptr<std::ostream> FileStreamPtr;
+
+  class PdalVectorMapWriter : public Writer
+  {
+  public:
+    PdalVectorMapWriter()
+    {}
+
+    static void  * create();
+    static int32_t destroy(void *);
+    std::string getName() const;
+
+  private:
+    virtual void addArgs(ProgramArgs& args);
+    virtual void initialize();
+    virtual void ready(PointTableRef table);
+    virtual void write(const PointViewPtr view);
+    virtual void done(PointTableRef table);
+
+    std::string m_filename;
+    std::string m_newline;
+    std::string m_datafield;
+    int m_precision;
+
+    FileStreamPtr m_stream;
+    Dimension::Id m_dataDim;
+  };
+
+} // namespace pdal
